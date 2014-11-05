24515b38496efb7997aade4fc49a4113fabbccfe
Kjetil Valle <kjetil.valle@gmail.com>
2014-11-05 23:54:12 +0100

    Split out repository module

---
+++ api/repository.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from flask.ext.restful import abort
+import flask
+import shelve
+
+from api import app
+
+@app.before_request
+def before_request():
+    flask.g.db = shelve.open('beers.db')
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(flask.g, 'db', False)
+    if db: 
+        db.close()
+
+def ucode(string):
+    "Hack because shelve dosn't support unicode keys"
+    return str(string).encode('utf-8')
+
+def get_beer(beer_id):
+    try:
+        return flask.g.db[ucode(beer_id)]
+    except KeyError:
+        abort(404, message="No beer found with beer_id {}".format(beer_id))
+
+def delete_beer(beer_id):
+    beer_id = ucode(beer_id)
+    try:
+        del flask.g.db[beer_id]
+    except KeyError:
+        abort(404, message="No beer with beer_id {} to delete".format(beer_id))
+
+def add_beer(beer):
+    bid = next_id()
+    beer["beer_id"] = bid
+    flask.g.db[bid] = beer
+    return flask.g.db[bid]
+
+def update_beer(beer_id, beer):
+    beer_id = ucode(beer_id)
+    beer["beer_id"] = beer_id
+    if beer_id not in flask.g.db:
+        abort(404, message="No beer found with beer_id {}".format(beer_id))
+    flask.g.db[beer_id] = beer
+    return beer
+
+def list_beers():
+    return flask.g.db.values()
+
+def next_id():
+    ids = [int(beer_id) for beer_id in flask.g.db.keys()]
+    return ucode(max(ids) + 1) if ids else ucode('1')
+
--- api/views.py
+++ api/views.py
@@ -1,63 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from api import api, app
-
-from flask.ext.restful import reqparse, abort, Api, Resource
+from flask.ext.restful import reqparse, Resource
 import flask
 
-
-import shelve
-
-@app.before_request
-def before_request():
-    flask.g.db = shelve.open('beers.db')
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(flask.g, 'db', False)
-    if db: 
-        db.close()
-
-def get_beer(beer_id):
-    try:
-        return flask.g.db[beer_id]
-    except KeyError:
-        abort(404, message="No beer found with beer_id {}".format(beer_id))
-
-def delete_beer(beer_id):
-    beer_id = str(beer_id) # stop when throwing away shelve
-    try:
-        del flask.g.db[beer_id]
-    except KeyError:
-        abort(404, message="No beer with beer_id {} to delete".format(beer_id))
-
-def add_beer(beer):
-    bid = next_id()
-    beer["beer_id"] = bid
-    flask.g.db[bid] = beer
-    return flask.g.db[bid]
-
-def update_beer(beer_id, beer):
-    beer_id = str(beer_id) # stop when throwing away shelve
-    if beer_id not in flask.g.db:
-        abort(404, message="No beer found with beer_id {}".format(beer_id))
-    flask.g.db[beer_id] = beer
-
-def list_beers():
-    return flask.g.db.values()
-
-def next_id():
-    ids = [int(beer_id) for beer_id in flask.g.db.keys()]
-    return str(max(ids) + 1) if ids else "1"
-
-# deprecated
-def abort_if_not_in_cellar(beer_id):
-    if beer_id not in flask.g.db:
-        abort(404, message="No beer with beer_id {} in the cellar".format(beer_id))        
-
-
-
-
+from api import api
+from api import repository as repo
 
 parser = reqparse.RequestParser()
 parser.add_argument('name', 
@@ -79,25 +26,24 @@ parser.add_argument(
 
 class Beer(Resource):
     def get(self, beer_id):
-        return get_beer(beer_id)
+        return repo.get_beer(beer_id)
 
     def delete(self, beer_id):
-        delete_beer(beer_id)
+        repo.delete_beer(beer_id)
         return '', 204
 
     def put(self, beer_id):
-        beer = parser.parse_args()
-        update_beer(beer_id, beer)
+        beer = repo.update_beer(beer_id, parser.parse_args())
         return beer, 201
 
 
 class BeerList(Resource):
     def get(self):
-        return list_beers()
+        return repo.list_beers()
 
     def post(self):
         beer = parser.parse_args()
-        saved_beer = add_beer(beer)
+        saved_beer = repo.add_beer(beer)
         return saved_beer, 201
 
 
