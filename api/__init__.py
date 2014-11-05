3aa494e0d2ec0f8aef0932b147c6b452f4c9ada2
Kjetil Valle <kjetil.valle@gmail.com>
2014-11-05 08:35:26 +0100

    Converted to flask-restful

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, make_response
+from flask.ext import restful
+
 import os
 
 app = Flask(__name__)
+api = restful.Api(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
--- api/views.py
+++ api/views.py
@@ -1,40 +1,62 @@
 # -*- coding: utf-8 -*-
 
-import flask
-from api import app
+from api import api
+from flask.ext.restful import reqparse, abort, Api, Resource
 
-@app.route('/beer')
-def list_beers():
-    data = { 
-        "items": mock_beers(),
-        "count": len(mock_beers())
-    }
-    return flask.make_response(flask.jsonify(data), 200)
+# repo
 
-@app.route('/beer/<int:ident>')
-def get_beer(ident):
-    data = mock_beers()[ident - 1]
-    return flask.make_response(flask.jsonify(data), 200)
-
-@app.route('/beer', methods=['POST'])
-def add_beer():
-    return flask.make_response('', 201)
-
-
-# Mocks
-
-def mock_beers():
-    return [
-        {
+BEERS = {
+        1: {
                     "id": 1,
                     "name": "Dark Horizon 3",
                     "brewery": "Nøgne Ø",
                     "count": 2
         },
-        {
+        2: {
                     "id": 2,
                     "name": "Unearthly Oak Aged",
                     "brewery": "Southern Tier",
                     "count": 1
         }
-    ]
+    }
+
+def abort_if_not_in_cellar(beer_id):
+    if beer_id not in BEERS:
+        abort(404, message="No beer with beer_id {} in the cellar".format(beer_id))
+
+# resources
+
+parser = reqparse.RequestParser()
+parser.add_argument('name', type=str) # moar here
+
+class Beer(Resource):
+    def get(self, beer_id):
+        abort_if_not_in_cellar(beer_id)
+        return BEERS[beer_id]
+
+    def delete(self, beer_id):
+        abort_if_not_in_cellar(beer_id)
+        del BEERS[beer_id]
+        return '', 204
+
+    def put(self, beer_id):
+        args = parser.parse_args()
+        beer = {'name': args['name']}
+        BEERS[beer_id] = beer
+        return beer, 201
+
+
+class BeerList(Resource):
+    def get(self):
+        return BEERS
+
+    def post(self):
+        args = parser.parse_args()
+        beer_id = len(BEERS) + 1
+        BEERS[beer_id] = {'name': args['name']}
+        return BEERS[beer_id], 201
+
+# views
+
+api.add_resource(BeerList, '/beers')
+api.add_resource(Beer, '/beers/<int:beer_id>')
