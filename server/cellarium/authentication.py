f4787a64f2314c19f6677857d204b9ff51cc747e
Kjetil Valle <kjetil.valle@gmail.com>
2015-03-02 21:01:11 +0100

    All fields are required for password change

--- server/cellarium/authentication.py
+++ server/cellarium/authentication.py
@@ -243,8 +243,6 @@ class Users(Resource):
         data = parser.parse_args()
 
         if not verify_password(data.username, data.password):
-            print data.username
-            print data.password
             return {"message": "Wrong password"}, 400
 
         if not is_acceptable_password(data.new_password):
